36792985df975b84118a87463cfd285c09948dd6
Dmitry <dmitry@iMac-Dmitry.lan>
2026-03-14 17:28:57 +0300

    не забыть протестировать

--- project/chat-server.c
+++ project/chat-server.c
@@ -60,8 +60,60 @@ void *client_handler(void *arg) {
     close(sock);
     return NULL;
 }
-int main(){
+int main() {
+    int server_sock, new_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    pthread_t thread_id;
 
+    signal(SIGINT, signal_handler);
 
+    // Создание сокета
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    // Настройка адреса
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(server_sock, MAX_CLIENTS) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("Сервер запущен на порту %d. Подключайтесь: telnet localhost %d\n", PORT, PORT);
+
+    while (server_running) {
+        new_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_len);
+        if (new_sock < 0) {
+            if (!server_running) break;
+            perror("accept");
+            continue;
+        }
+
+        pthread_mutex_lock(&clients_mutex);
+        if (num_clients < MAX_CLIENTS) {
+            client_socks[num_clients++] = new_sock;
+            int *new_sock_ptr = malloc(sizeof(int));
+            *new_sock_ptr = new_sock;
+            pthread_create(&thread_id, NULL, client_handler, new_sock_ptr);
+            printf("Новый клиент подключён: %s:%d (всего %d)\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), num_clients);
+        } else {
+            close(new_sock);
+        }
+        pthread_mutex_unlock(&clients_mutex);
+    }
+
+    close(server_sock);
+    pthread_mutex_destroy(&clients_mutex);
     return 0;
 }

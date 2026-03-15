929899889d0a20a3b5937116ba212c1005292be6
Dmitry <dmitry@iMac-Dmitry.lan>
2026-03-15 15:28:36 +0300

    Добавлен подсчет рабочего времени,если присутствуют ночные смены

--- project/Tabel/tabel.c
+++ project/Tabel/tabel.c
@@ -11,14 +11,23 @@ int main() {
 
     printf("Укажите стоимость часа.\n");
     scanf("%f", &price);
+
     printf("Укажите начало рабочего дня и конец.\n");
     scanf("%f:%f %f:%f", &hour1, &min1, &hour2, &min2);
+
     hour1 = hour1 + min1 / 60.0;
     hour2 = hour2 + min2 / 60.0;
+
+    if (hour2 < hour1) {
+        hour2 += 24.0;
+    }
+
     total_hours = hour2 - hour1;
     total_price = total_hours * price;
+
     conv_hours = (int) total_hours;
     conv_min = (int)((total_hours - conv_hours) * 60);
+
     printf("Отработано:%d ч %d мин, Заработано:%.2f\n", conv_hours, conv_min, total_price);
     fprintf(file, "Часы %d Мин %d, Зп: %.2f\r\n", conv_hours, conv_min, total_price);
     fclose(file);
